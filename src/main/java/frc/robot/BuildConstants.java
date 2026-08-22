4aa49717ebebfcdf52cb12360bf8b585258da7e6
Dominic Nguyen <dominic.j.nguyen@gmail.com>
2026-08-21 17:59:23 -0700

    updated robot MOI and width in constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026CC-Advanced";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "a7769206f35289d51301afd910017ec7cc46a6a2";
-  public static final String GIT_DATE = "2026-08-07 13:13:12 PDT";
+  public static final int GIT_REVISION = 14;
+  public static final String GIT_SHA = "0075dfa269fba5c4ae2c3534b3aa9444d1046071";
+  public static final String GIT_DATE = "2026-08-09 12:37:46 PDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-08-07 13:13:55 PDT";
-  public static final long BUILD_UNIX_TIME = 1786133635103L;
+  public static final String BUILD_DATE = "2026-08-09 12:38:30 PDT";
+  public static final long BUILD_UNIX_TIME = 1786304310968L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
