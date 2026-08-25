5fad7b7201b724884fb0f603421d2cb567231be7
Dominic Nguyen <dominic.j.nguyen@gmail.com>
2026-08-25 16:19:54 -0700

    updated tuner constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026CC-Advanced";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "a7769206f35289d51301afd910017ec7cc46a6a2";
-  public static final String GIT_DATE = "2026-08-07 13:13:12 PDT";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "52c70ef2dd423617b4e041279701a01b68ca877b";
+  public static final String GIT_DATE = "2026-08-07 13:15:08 PDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-08-07 13:13:55 PDT";
-  public static final long BUILD_UNIX_TIME = 1786133635103L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-08-25 16:19:04 PDT";
+  public static final long BUILD_UNIX_TIME = 1787699944150L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
