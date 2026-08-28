c5158d452193d883247aee6f604b3181e47bc94f
Dominic Nguyen <dominic.j.nguyen@gmail.com>
2026-08-28 09:42:08 -0700

    updated robot weight

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026CC-Advanced";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 14;
-  public static final String GIT_SHA = "0075dfa269fba5c4ae2c3534b3aa9444d1046071";
-  public static final String GIT_DATE = "2026-08-09 12:37:46 PDT";
+  public static final int GIT_REVISION = 18;
+  public static final String GIT_SHA = "b094377f9abb415e0e6d9cac7358e1e368ff201b";
+  public static final String GIT_DATE = "2026-08-25 16:20:38 PDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-08-09 12:38:30 PDT";
-  public static final long BUILD_UNIX_TIME = 1786304310968L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-08-25 16:23:45 PDT";
+  public static final long BUILD_UNIX_TIME = 1787700225034L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
